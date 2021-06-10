f7f64cbfd30e3b374f1081d7fb745aa6d3c7a068
mhacode <mainulcodemen@gmail.com>
2021-06-10 21:12:56 +0600

    First commit

---
+++ 01/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:0@localhost/bookstore?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("You are connected to the databases")
+
+}
